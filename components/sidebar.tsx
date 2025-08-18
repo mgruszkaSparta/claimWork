32602b7fc86d75724f2d353315a2daf39d8740cf
mgruszkaSparta <marcin.gruszka@spartabrokers.pl>
2025-08-18 12:56:22 +0200

    fix: import missing bar chart icon

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
 
-import { LayoutDashboard, FileText, Car, Settings, Inbox, Calendar, CalendarCheck } from "lucide-react"
+import { LayoutDashboard, FileText, Car, Settings, Inbox, Calendar, CalendarCheck, BarChart3 } from "lucide-react"
 
 
 import { useAuth } from "@/hooks/use-auth"

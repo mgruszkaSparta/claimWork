393cd8740cd1c09249287d51d41bf655f4b08d3e
mgruszkaSparta <marcin.gruszka@spartabrokers.pl>
2025-08-20 18:24:47 +0200

    fix: wrap auth provider jsx

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -75,7 +75,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     logout,
   }
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  )
 }
 
 export function useAuth() {

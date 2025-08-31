fea6e7684ca62e401bdbce62d91a268cfef0bdb1
mgruszkaSparta <marcin.gruszka@spartabrokers.pl>
2025-09-01 01:40:14 +0200

    Wrap mobile app with notifications provider

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { NotificationsProvider } from "../../mobile/hooks/useNotifications";
 
 const MobileApp = dynamic(() => import("../../mobile/App"), { ssr: false });
 
 export default function MobilePage() {
-  return <MobileApp />;
+  return (
+    <NotificationsProvider>
+      <MobileApp />
+    </NotificationsProvider>
+  );
 }
